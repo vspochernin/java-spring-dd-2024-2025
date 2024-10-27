347b903a7a385dbe2ab49159613fc7a8673bdfb9
Vladislav Pochernin <vspochernin@gmail.com>
2024-10-27 14:36:00 +0300

    lab-1.1: fix Main

--- lab-1.1/src/main/java/ru/vspochernin/Main.java
+++ lab-1.1/src/main/java/ru/vspochernin/Main.java
@@ -14,23 +14,23 @@ public class Main {
     public static void main(String[] args) {
         StudentCollection studentCollection =
                 new StudentCollection(List.of(
-                        new Student("Name1", "Group1", 3, List.of(1, 2, 3, 3, 3)),
-                        new Student("Name2", "Group1", 3, List.of(2, 2, 3, 3, 5)),
-                        new Student("Name3", "Group2", 4, List.of(3, 3, 3, 4, 5)),
-                        new Student("Name4", "Group2", 4, List.of(4, 4, 4, 4, 5)),
-                        new Student("Name5", "Group3", 5, List.of(5, 5, 5, 5, 5))));
+                        new Student("Имя1", "Группа1", 3, List.of(1, 2, 3, 3, 3)),
+                        new Student("Имя2", "Группа1", 3, List.of(2, 2, 3, 3, 5)),
+                        new Student("Имя3", "Группа2", 4, List.of(3, 3, 3, 4, 5)),
+                        new Student("Имя4", "Группа2", 4, List.of(4, 4, 4, 4, 5)),
+                        new Student("Имя5", "Группа3", 5, List.of(5, 5, 5, 5, 5))));
 
-        System.out.println("Students before next course");
+        System.out.println("Студенты до перевода на следующий курс:");
         PrintUtils.printStudents(studentCollection.getStudents());
         System.out.println();
 
         studentCollection.toTheNextCourse();
 
-        System.out.println("Students after next course");
+        System.out.println("Студенты после перевода на следующий курс:");
         PrintUtils.printStudents(studentCollection.getStudents());
         System.out.println();
 
-        System.out.println("Students on the 2 course");
+        System.out.println("Студенты 5-го курса:");
         PrintUtils.printStudents(studentCollection.getStudents(), 5);
         System.out.println();
     }
