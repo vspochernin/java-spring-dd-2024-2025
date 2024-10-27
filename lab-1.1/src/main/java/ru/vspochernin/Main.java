3f8a23140a164f2b6db7fb95a8a72f4620486c1c
Vladislav Pochernin <vspochernin@gmail.com>
2024-10-27 14:34:38 +0300

    lab-1.1: implement Main

--- lab-1.1/src/main/java/ru/vspochernin/Main.java
+++ lab-1.1/src/main/java/ru/vspochernin/Main.java
@@ -1,10 +1,37 @@
 package ru.vspochernin;
 
+import java.util.List;
+
+import ru.vspochernin.collection.StudentCollection;
+import ru.vspochernin.model.Student;
+import ru.vspochernin.utils.PrintUtils;
+
 /**
  * @author pochernin-vla
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StudentCollection studentCollection =
+                new StudentCollection(List.of(
+                        new Student("Name1", "Group1", 3, List.of(1, 2, 3, 3, 3)),
+                        new Student("Name2", "Group1", 3, List.of(2, 2, 3, 3, 5)),
+                        new Student("Name3", "Group2", 4, List.of(3, 3, 3, 4, 5)),
+                        new Student("Name4", "Group2", 4, List.of(4, 4, 4, 4, 5)),
+                        new Student("Name5", "Group3", 5, List.of(5, 5, 5, 5, 5))));
+
+        System.out.println("Students before next course");
+        PrintUtils.printStudents(studentCollection.getStudents());
+        System.out.println();
+
+        studentCollection.toTheNextCourse();
+
+        System.out.println("Students after next course");
+        PrintUtils.printStudents(studentCollection.getStudents());
+        System.out.println();
+
+        System.out.println("Students on the 2 course");
+        PrintUtils.printStudents(studentCollection.getStudents(), 5);
+        System.out.println();
     }
 }
