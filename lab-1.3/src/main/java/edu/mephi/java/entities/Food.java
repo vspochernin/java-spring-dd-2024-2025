74dd8ec1a0a98afca838b05cb6ee9d81725a5287
Vladislav Pochernin <vspochernin@gmail.com>
2024-11-15 20:23:39 +0300

    lab-1.3: add Food class

---
+++ lab-1.3/src/main/java/edu/mephi/java/entities/Food.java
@@ -0,0 +1,22 @@
+package edu.mephi.java.entities;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Food {
+
+    private Point location;
+
+    public Food() {
+        spawn();
+    }
+
+    public void spawn() {
+        Random random = new Random();
+        location = new Point(random.nextInt(20), random.nextInt(20));
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+}
