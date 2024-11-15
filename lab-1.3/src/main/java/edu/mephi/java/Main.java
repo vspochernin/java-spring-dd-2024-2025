9d107f28c9a0f69191c558e9bd3ad25c3800ac48
Vladislav Pochernin <vspochernin@gmail.com>
2024-11-15 21:11:32 +0300

    lab-1.3: finalize Main

--- lab-1.3/src/main/java/edu/mephi/java/Main.java
+++ lab-1.3/src/main/java/edu/mephi/java/Main.java
@@ -2,22 +2,23 @@ package edu.mephi.java;
 
 import edu.mephi.java.engine.Game;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Game");
+        JFrame frame = new JFrame("Snake");
         Game game = new Game();
+
         frame.add(game);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
         game.addKeyListener(new KeyAdapter() {
+
             @Override
             public void keyPressed(KeyEvent e) {
                 if (game.isGameOver() && e.getKeyCode() == KeyEvent.VK_R) {
@@ -25,6 +26,5 @@ public class Main {
                 }
             }
         });
-
     }
 }
