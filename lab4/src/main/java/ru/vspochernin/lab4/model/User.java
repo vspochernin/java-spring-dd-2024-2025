8f7b7addb62366b16e98ac8ef0ddb8efcd007f99
Vladislav Pochernin <vspochernin@gmail.com>
2025-06-28 20:43:47 +0300

    lab4: add console

--- lab4/src/main/java/ru/vspochernin/lab4/model/User.java
+++ lab4/src/main/java/ru/vspochernin/lab4/model/User.java
@@ -1,15 +1,17 @@
 package ru.vspochernin.lab4.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
