e0017c9223a57880fad3aaf1514c8befe923d988
Vladislav Pochernin <vspochernin@gmail.com>
2025-06-28 20:38:28 +0300

    lab4: add model classes

---
+++ lab4/src/main/java/ru/vspochernin/lab4/model/Role.java
@@ -0,0 +1,10 @@
+package ru.vspochernin.lab4.model;
+
+public enum Role {
+
+    ADMIN,
+    DEVELOPER,
+    TESTER,
+    SYSTEM_ANALYST,
+    TEAM_LEAD
+}
---
+++ lab4/src/main/java/ru/vspochernin/lab4/model/User.java
@@ -0,0 +1,28 @@
+package ru.vspochernin.lab4.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
